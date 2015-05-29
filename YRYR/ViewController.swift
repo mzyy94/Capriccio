f20be1f7e86aa3495a2dabd81c719de89284a9a7
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-05-29 22:37:41 +0900

    Initial Commit

---
+++ YRYR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YRYR
+//
+//  Created by Yuki MIZUNO on 5/29/15.
+//  Copyright (c) 2015 Yuki MIZUNO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
