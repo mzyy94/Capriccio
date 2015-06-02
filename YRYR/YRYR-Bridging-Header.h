9f6f0065c717fcfc3a74eac2e557908497f8c262
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-06-02 19:17:33 +0900

    Add ObjC bridging header file to use MobileVLCKit framework

---
+++ YRYR/YRYR-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  YRYR-Bridging-Header.h
+//  YRYR
+//
+//  Created by Yuki MIZUNO on 6/2/15.
+//  Copyright (c) 2015 Yuki MIZUNO. All rights reserved.
+//
+
+#ifndef YRYR_Bridging_Header_h
+#define YRYR_Bridging_Header_h
+
+#import <MobileVLCKit/MobileVLCKit.h>
+
+
+#endif
